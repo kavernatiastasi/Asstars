c44a880dda4b4943f537e514b6e55cc9311d9c6e
kavernatiastasi <65601452+kavernatiastasi@users.noreply.github.com>
2025-05-11 21:56:48 +0100

    Update AutoDonate.js

--- AutoDonate.js
+++ AutoDonate.js
@@ -50,7 +50,7 @@
     const button = document.createElement('button');
     document.body.appendChild(button);
     Object.assign(button.style, { /* ... стилі кнопки ... */
-        position: 'fixed', top: '65px', left: '120px', zIndex: '10000',
+        position: 'fixed', top: '65px', left: '210px', zIndex: '10000',
         padding: '12px 20px', fontSize: '16px', color: 'white', border: 'none',
         borderRadius: '50px', cursor: 'pointer', fontFamily: '"Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif',
         boxShadow: '0 6px 12px rgba(0, 0, 0, 0.2), 0 0 0 0 rgba(52, 152, 219, 0.0)',
